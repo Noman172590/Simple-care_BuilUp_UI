3e4594b9524418304d22613e91d21cdd6c72357d
Noman Jahan <nomanjahan866615@gmail.com>
2023-05-17 20:03:18 +0600

    live_link,html,css and javascript file uploade

---
+++ index.js
@@ -0,0 +1,74 @@
+const data = [
+    {
+      _id: "60795d4e0489a32f948c4167",
+      name: "Honda Sedan",
+      price: "132",
+      description:
+        "Vehicle Type: Sedan, Doors: 2, Seats: 2, Luggage: 2 Suitcases / 2 Bags, Transmission: Automatic",
+      imageURL: "https://i.ibb.co/54WzQjR/Honda.png",
+    },
+    {
+      _id: "60795e440489a32f948c4168",
+      name: "MitoSedan",
+      price: "221",
+      description:
+        "Vehicle Type: Sedan, Doors: 2, Seats: 2, Luggage: 2 Suitcases / 2 Bags, Transmission: Manual",
+      imageURL: "https://i.ibb.co/802Rwsq/Mito.png",
+    },
+    {
+      _id: "60795fc20489a32f948c4169",
+      name: "Isuzu Tacoma",
+      price: "105",
+      description:
+        "Vehicle Type: Pickup Truck, Doors: 5, Seats: 4, Luggage: 6 Suitcases / 8 Bags, Transmission: Manual",
+      imageURL: "https://i.ibb.co/SJK7QYs/isuzu.png",
+    },
+    {
+      _id: "6079615d0489a32f948c416a",
+      name: "Chevrolet Crossover",
+      price: "434",
+      description:
+        "Vehicle Type: Crossover, Doors: 5, Seats: 7, Luggage: 5Suitcases / 5Bags, Transmission: Automatic",
+      
+    },
+  ];
+
+  const Honda_sedan=document.getElementById('Honda_sedan')
+  const Mitosedan=document.getElementById('Mitosedan')
+  console.log(data)
+
+  const div=document.createElement('div')
+  div.classList.add('col')
+  div.innerHTML=`
+
+  <div class="card">
+  <img src="${data[0]. imageURL}" class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">Car Name:${data[0].name}</h5>
+    <p class="card-text">car detail:${data[0].description
+    }</p>
+  </div>
+
+
+  
+  `
+  Honda_sedan.appendChild(div)
+
+
+  const cretediv2=document.createElement('div')
+  cretediv2.innerHTML=`
+  
+  <div class="card">
+  <img src="${data[1]. imageURL}" class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">Car Name:${data[1].name}</h5>
+    <p class="card-text">car detail:${data[1].description
+    }</p>
+  </div>
+  
+  
+  `
+  
+  Mitosedan.appendChild(cretediv2)
+
+ 
